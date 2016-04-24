bcf3bf33b1d69e93f0514c0ae660713925e47e52
vishnusak <vishnusak@gmail.com>
2016-04-24 09:35:58 -0700

    Introduced ghost to the game.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+##Updates - 24-April:
+- Slightly modified the grid
+- Introduced ghost
+  - moves independant of pacman
+  - doesn't go through walls
+  - kills pacman if they meet
+- game over message displayed
+- ToDo improvement: make ghost target pacman. Right now it just moves randomly, so its not much of a threat
+
 ##FEATURE WISH LIST
 
 1. Collision detection (PacMan shouldn't walk through walls!) - DONE
@@ -6,7 +15,7 @@
 4. Reorient PacMan according to direction of movement         - DONE
 
 ###HACKER FEATURE:
-5. Add ghosts that move independently w/ collision detection
+5. Add ghosts that move independently w/ collision detection  - Partially DONE
 
 
 Either **fork** or **clone** this repository, code some of the features, and then either push to your own repository *or* send me a **pull request**.
--- scripts/pacman.js
+++ scripts/pacman.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   // showMenu();
+  findEm();
   drawGameBoard();
+  startGhost();
   addKeyPressListener();
 })
 
@@ -16,55 +18,108 @@ function buildBoard(){
 // using HTML data methods to build and store the board representations
 }
 
+// flag to represent if pacman is alive or not
+var alive = true;
+
+// the scheduler interval ID generated when ghostProtocol is started. Used to stop ghostProtocol
+var gp;
+
+// references of the different entities in the board
 var BRK = 0;
 var COI = 1;
 var PAC = 2;
 var EMP = 3;
+var GHS = 4;
 
 // 24 x 33 grid
 var gameBoardGrid = [
 // 0  1   2   3   4   5   6   7   8   9   0   1   2   3   4   5   6   7   8   9   0   1   2   3   4   5   6   7   8   9   0   1   2
-[BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,EMP,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK],
-[BRK,PAC,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,EMP,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK],
-[BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK],
-[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK],
-[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK],
-[BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK],
-[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,COI,BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK,COI,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK],
-[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK],
-[EMP,BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,COI,BRK,EMP,BRK,COI,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK,EMP],
-[EMP,BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK,EMP],
-[BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK],
-[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK],
-[BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK],
-[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK],
-[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK],
-[BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK],
-[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,EMP,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK],
-[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK],
-[BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,EMP,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK],
+[BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,EMP,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK], // 0
+[BRK,PAC,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 1
+[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,EMP,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK], // 2
+[BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK], // 3
+[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK], // 4
+[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 5
+[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK], // 6
+[BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK], // 7
+[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK], // 8
+[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 9
+[BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,COI,BRK,COI,BRK,COI,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK], // 0
+[EMP,BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,COI,COI,GHS,COI,COI,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK,EMP], // 1
+[EMP,BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,COI,BRK,COI,BRK,COI,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK,EMP], // 2
+[BRK,BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK,BRK], // 3
+[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 4
+[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK], // 5
+[BRK,COI,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,EMP,EMP,BRK,COI,BRK,COI,BRK], // 6
+[BRK,COI,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,BRK,COI,BRK,BRK,BRK,BRK,COI,BRK,COI,BRK], // 7
+[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 8
+[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK], // 9
+[BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,BRK,COI,BRK,EMP,EMP,EMP,EMP,BRK,COI,BRK,EMP,EMP,EMP,BRK,COI,BRK], // 0
+[BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,EMP,BRK,COI,BRK,BRK,BRK,BRK,BRK,BRK,COI,BRK,BRK,BRK,BRK,BRK,COI,BRK], // 1
+[BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK,EMP,BRK,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,COI,BRK], // 2
+[BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,EMP,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK,BRK], // 3
                     ];
 
+// will hold the current position of pacman
 var pacman = {
-  x: 1,
-  y: 1
+  x: 0,
+  y: 0
 }
-// previous position of pacman
-var prevX = 1;
-var prevY = 1;
+// will hold the previous position of pacman
+var prevPac = {
+  x: 0,
+  y: 0
+}
 
 // direction of pacman to orient the image
 var dir   = 0;
 
+// will hold the current position of the ghost. 
+var ghost = {
+  x: 0,
+  y: 0
+}
+// previous direction of movement of ghost (left, right, up, down)
+var prevGhostStep = '';
+
+// original content of the cell occupied by the ghost
+var ghostCellContent = COI;
+
 // scorecard
 var score = 0;
 
+// locate pacman and ghost at the beginning of the game. 
+// This will allow us to change the hardcoded location of the characters in the grid as we please without worrying about updating their location variables/objects
+
+function findEm(){
+  for (var rowIdx = 0; rowIdx < gameBoardGrid.length; rowIdx++){
+    for (var cellIdx = 0; cellIdx < gameBoardGrid[rowIdx].length; cellIdx++){
+      if (gameBoardGrid[rowIdx][cellIdx] == 2){
+        pacman.x = cellIdx;
+        pacman.y = rowIdx;
+        prevPac.x = cellIdx;
+        prevPac.y = rowIdx;
+        console.log("pacman: row-" + rowIdx + " col-" + cellIdx);
+      }
+
+      if (gameBoardGrid[rowIdx][cellIdx] == 4){
+        ghost.x = cellIdx;
+        ghost.y = rowIdx;
+        console.log("ghost: row-" + rowIdx + " col-" + cellIdx);
+      }
+    }
+  }
+}
+
 function drawGameBoard(){
+  if (!alive){return}; // Added this so that after game over we dont want any redrawing of the board
+
+  if ((ghost.x == pacman.x) && (ghost.y == pacman.y)){
+    console.log("Pacman is DEAD!!!");
+    alive = false;
+    clearInterval(gp);
+  }
+
   var htmlElements = [];
 
   for (var rowIdx = 0; rowIdx < gameBoardGrid.length; rowIdx++){
@@ -75,6 +130,8 @@ function drawGameBoard(){
   }
 
   $('.game-board').html(htmlElements);
+
+  // Adding the scorecard
   $('.score').html($('<ul />',{
     html: $('<li />',{
       text: "Score"
@@ -83,6 +140,13 @@ function drawGameBoard(){
       text: score
     })
   }))
+
+  // showing the game over message
+  if (!alive){
+    $('.score ul').append($('<li />',{
+      text: "Game Over"
+    }))
+  }
 }
 
 function fetchRowCells(row){
@@ -99,30 +163,37 @@ function fetchRowCells(row){
         className = 'coin';
         break;
       case 2:
-        console.log("in class name setter." + dir);
-        switch (dir){
-          case 37:
-            className = 'pacman-l';
-            break;
-          case 38:
-            className = 'pacman-u';
-            break;
-          case 0:
-          case 39:
-            className = 'pacman-r';
-            break;
-          case 40:
-            className = 'pacman-d';
-            break;
-          default:
-            console.log('Arrow direction indeterminate!');
+        if (!alive){
+          className = 'dead'
+        } else {
+          switch (dir){
+            case 37:
+              className = 'pacman-l';
+              break;
+            case 38:
+              className = 'pacman-u';
+              break;
+            case 0:
+            case 39:
+              className = 'pacman-r';
+              break;
+            case 40:
+              className = 'pacman-d';
+              break;
+            default:
+              console.log('Arrow direction indeterminate!');
+          }
         }
         // className = 'pacman';
         break;
       case 3:
         className = 'empty';
         break;
+      case 4:
+        className = 'ghost';
+        break;
       default:
+        // The below will not be triggered becaue our board is hardcoded. Added this for future use when board can be dynamically generated
         alert('Invalid value for bulding the Board. Check log');
         console.log('fetchRowCells: Invalid Row[IDX] value used = ' + row[cellIdx]);
     }
@@ -136,9 +207,11 @@ function fetchRowCells(row){
 
 
 function addKeyPressListener(){
+  if (!alive){return}; // Added this so that after game over keyPress is not processed
+
   $(document).keydown(function(event){
-    prevX = pacman.x;
-    prevY = pacman.y;
+    prevPac.x = pacman.x;
+    prevPac.y = pacman.y;
     switch(event.which){
       case 37:
         dir = 37;
@@ -157,7 +230,11 @@ function addKeyPressListener(){
         pacman.y +=1;
         break;
       default:
-        alert('Invalid key. Please use Arrow keys');
+        // uncomment the alert and comment out the console.log if that logging is not desired
+        // comment out both alert and console.log if no message has to be sent to the user
+
+        // alert('Invalid key. Please use Arrow keys');
+        console.log('Invalid key. Please use Arrow keys');
         return;
     }
 
@@ -166,11 +243,66 @@ function addKeyPressListener(){
         score++;
       }
       gameBoardGrid[pacman.y][pacman.x] = PAC;
-      gameBoardGrid[prevY][prevX] = EMP;
+      gameBoardGrid[prevPac.y][prevPac.x] = EMP;
     } else {
-      pacman.x = prevX;
-      pacman.y = prevY;
+      pacman.x = prevPac.x;
+      pacman.y = prevPac.y;
     }
     drawGameBoard();
   })
+}
+
+function startGhost(){
+  gp = setInterval(ghostProtocol, 200); // change the number here to speed up or slow down the ghost
+}
+
+function ghostProtocol(){
+  var dirAvail = [];
+
+  if(gameBoardGrid[ghost.y][ghost.x - 1] !== BRK){dirAvail.push('l')};
+  if(gameBoardGrid[ghost.y][ghost.x + 1] !== BRK){dirAvail.push('r')};
+  if(gameBoardGrid[ghost.y - 1][ghost.x] !== BRK){dirAvail.push('u')};
+  if(gameBoardGrid[ghost.y + 1][ghost.x] !== BRK){dirAvail.push('d')};
+
+  // The below will make sure that if the ghost has multiple options for taking the next step, it doesn't retrace its step. This stops the ghost from oscillating between two adjacent cells and makes it move through the grid
+  if ((prevGhostStep == '') || (dirAvail.length == 1)){
+    //  do nothing if these conditions are satisfied
+  } else {
+    if ((prevGhostStep == 'l') && ($.inArray('r',dirAvail) > -1)){dirAvail.splice($.inArray('r', dirAvail),1);}
+    if ((prevGhostStep == 'r') && ($.inArray('l',dirAvail) > -1)){dirAvail.splice($.inArray('l', dirAvail),1);}
+    if ((prevGhostStep == 'u') && ($.inArray('d',dirAvail) > -1)){dirAvail.splice($.inArray('d', dirAvail),1);}
+    if ((prevGhostStep == 'd') && ($.inArray('u',dirAvail) > -1)){dirAvail.splice($.inArray('u', dirAvail),1);}
+  }
+
+  var next = Math.floor(Math.random() * dirAvail.length);
+  
+  gameBoardGrid[ghost.y][ghost.x] = ghostCellContent;
+
+  switch (dirAvail[next]){
+    case 'l':
+      prevGhostStep = 'l';
+      ghost.x -= 1;
+      break;
+    case 'r':
+      prevGhostStep = 'r';
+      ghost.x += 1;
+      break;
+    case 'u':
+      prevGhostStep = 'u';
+      ghost.y -= 1;
+      break;
+    case 'd':
+      prevGhostStep = 'd';
+      ghost.y += 1;
+      break;
+  }
+
+  if ((ghost.x == pacman.x) && (ghost.y == pacman.y)){
+    // do not update the ghosts position if it reaches pacman. This 
+  } else {
+    ghostCellContent = gameBoardGrid[ghost.y][ghost.x];
+    gameBoardGrid[ghost.y][ghost.x] = GHS;
+  }
+
+  drawGameBoard();
 }
